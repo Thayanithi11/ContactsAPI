f4f1147bccb8f0db6ea64f6be4b8034dac567855
Thayanithi <thayanithi452004@gmail.com>
2025-06-01 20:21:58 +0530

    comment in error.js

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,5 @@
 const {constants}=require('../constants')
-//this is newbranch
+//this is main
 const errorHandler=(err,req,res,next)=>{
     const statusCode = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
      switch(statusCode){
